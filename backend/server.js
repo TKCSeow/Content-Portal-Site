73d7faf95772160581e92e005de2313d995edd99
TKCSeow <33025216+TKCSeow@users.noreply.github.com>
2020-01-10 09:02:36 +0000

    Mongo connection working, added user count, added timestamp, added loading messages, started Mocha Chai tests

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
+//******************************* */
+//Node Server
+//******************************* */
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var mongo = require('mongodb').MongoClient;
-
+const mongoose = require('mongoose')
+const url = 'mongodb://127.0.0.1:27017/messages'
 var database;
-// var Message = mongoose.model('Message',{
-//     msg:String
-// });
 
+var userCount = 0;
+var messages = [];
+
+//******************************* */
+//Express
+//******************************* */
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -17,39 +24,56 @@ app.use(function(req,res,next){
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 })
 
-// app.postMessage('/'), function (req,res){
-//   console.log(req.body);
-//   var message = new Message(req.body);
+//******************************* */
+//Mongo
+//******************************* */
+mongoose.connect(url, { useNewUrlParser: true });
 
-// message.save();
+const db = mongoose.connection
+db.once('open', _ => {
+  console.log('Database connected:', url)
+})
 
-//   res.status(200);
-// }
+db.on('error', err => {
+  console.error('connection error:', err)
+})
 
-mongo.connect("mongodb://localhost:27017/test", { useNewUrlParser: true , useUnifiedTopology: true }), function(err, db){
-  if(!err){
-    console.log("we are connected to mongo");
-    database = db;
-   
-  }
-}
+//******************************* */
+//Websockets
+//******************************* */
 
 io.on('connection', function(socket){
+  //On Connect
+  console.log('a user connected');
+  userCount++;
+  io.emit('user count', userCount);
+
+  //Send Old Messages
+  io.emit('load messages', messages);
+
+  //Receive a message
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    messages.push(msg); //add to list
+    io.emit('chat message', msg); //send to everyone
   });
 
+  //Typing Feedback
   socket.on('typing', function(data){
     // console.log('someone is typing');
     io.emit('typing', data);
 });
-  console.log('a user connected');
+
+  
+  //On Disconnect
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    userCount--;
+    io.emit('user count', userCount);
   });
   
 });
 
+
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on :3000');
 });
